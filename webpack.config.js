60e9861d654cb8e0582db23aad3e558d0d32f604
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-09-19 14:11:36 +0300

    Configure webpack to load all required project files

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,38 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // installed via npm
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // installed via npm
+const webpack = require('webpack'); // to access built-in plugins
 const path = require('path');
-const merge = require("webpack-merge");
-const base = require("./base");
-const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devtool: "eval-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: [/\.vert$/, /\.frag$/],
-        use: "raw-loader"
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg|xml)$/i,
-        use: "file-loader"
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin({
-      root: path.resolve(__dirname, "../")
-    }),
-    new webpack.DefinePlugin({
-      CANVAS_RENDERER: JSON.stringify(true),
-      WEBGL_RENDERER: JSON.stringify(true)
-    }),
-    new HtmlWebpackPlugin({
-      template: "./index.html"
-    })
-  ]
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        /**
+         * With zero configuration,
+         *   clean-webpack-plugin will remove files inside the directory below
+         */
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+              test: [/\.vert$/, /\.frag$/],
+              use: "raw-loader"
+            },
+            {
+              test: /\.(gif|png|jpe?g|svg|xml)$/i,
+              use: "file-loader"
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({ template: './src/index.html' }),
+    ],
 };
-
-module.exports = merge(base, {
-  mode: "production",
-  output: {
-    filename: "bundle.min.js"
-  },
-  devtool: false,
-  performance: {
-    maxEntrypointSize: 900000,
-    maxAssetSize: 900000
-  },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          output: {
-            comments: false
-          }
-        }
-      })
-    ]
-  }
-});
