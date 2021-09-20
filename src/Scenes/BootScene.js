d45280fa3231b0380dc9192c1307e45383e1e85d
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-09-21 00:11:24 +0300

    Add and display zenva logo and progress bar

--- src/Scenes/BootScene.js
+++ src/Scenes/BootScene.js
@@ -7,10 +7,10 @@ export default class BootScene extends Phaser.Scene {
   };
 
   preload() {
-
+    this.load.image('logo', '../assets/zenva_logo.png');
   };
 
   create() { 
-
+    this.scene.start('Preloader')
   };
 };
