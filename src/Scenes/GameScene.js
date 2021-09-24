5b7fde315c1e0b3a4398ded3e70f066b6df38a84
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-09-24 14:21:56 +0300

    Add the rabbit player

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,6 +1,7 @@
 import 'phaser';
 import background from '../assets/bg_layer1.png'
 import platform from '../assets/ground_grass.png'
+import bunny_stand from '../assets/bunny1_stand.png'
 
 export default class GameScene extends Phaser.Scene {
 
@@ -11,13 +12,14 @@ export default class GameScene extends Phaser.Scene {
   preload() {
     this.load.image('bg', background);
     this.load.image('platform', platform)
+    this.load.image('bunny_stand', bunny_stand)
   };
 
   create() {
     this.add.image(400, 300, 'bg');
 
     const platforms = this.physics.add.staticGroup();
-    for (let i = 0; i < 5; ++i) {
+    for (let i = 0; i < 6; ++i) {
       const x = Phaser.Math.Between(80, 400);
       const y = 200 * i
       const platform = platforms.create(x, y, 'platform')
@@ -27,6 +29,7 @@ export default class GameScene extends Phaser.Scene {
       body.updateFromGameObject()
     }
 
+    this.physics.add.sprite(240, 320, 'bunny_stand').setScale(0.4)
 
 
   };
