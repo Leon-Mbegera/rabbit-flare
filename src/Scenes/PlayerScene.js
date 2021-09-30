7a57ac784cbb2f8c7b293df789cf45c332735802
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-09-30 12:50:11 +0300

    Build scene to take player name

---
+++ src/Scenes/PlayerScene.js
@@ -0,0 +1,21 @@
+import 'phaser'
+import config from '../Config/config'
+
+export default class Player extends Phaser.Scene {
+
+  constructor() {
+    super('player')
+  }
+
+  create() {
+    const btn = document.getElementById('btn')
+    btn.addEventListener('click', () => {
+      e.prevent();
+      const name = document.getElementById('name-input').value 
+      if (name !== '') {
+        config.player = name
+        this.scene.start('Game')
+      }
+    });
+  }
+}
