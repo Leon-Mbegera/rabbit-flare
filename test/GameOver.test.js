27eb2d968b4240343d037d1bf75e0fc093483f12
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-10-06 23:02:00 +0300

    Test player response object

--- test/GameOver.test.js
+++ test/GameOver.test.js
@@ -1,39 +1,43 @@
-import { fetchScore, endpoint } from '../src/Scenes/GameOver'
-import * as axios from 'axios';
+import { fetchScore } from '../src/Scenes/GameOver'
 
+const result = fetchScore();
 
-describe('Fetching players and their score onto leaderboard', () => {
-
-    describe('successful get request made to endpoint', () => {
-      const result = fetchScore();
-      
+    describe('successful get request made to endpoint', () => {      
       it('should return each player name and score', async () => {
-        
-
-       expect(Array.isArray(result)).toBeTruthy
+      expect(Array.isArray(result)).toBeTruthy
       }); 
+    })
 
-      it('asdf', () => {
+    describe('Tests the object response', () => {
+
+      it('returns an object with string and number properties', () => {
         if(result.length){
           for(let player of result){
             expect(typeof player).toEqual("object")
             expect(typeof player.user).toEqual("string")
             expect(typeof player.score).toEqual("number")
+          }
+        }
+        
+      })
+    })
+
+    describe('Tests for truthiness', () => {
+
+      it('returns a players properties as truthy', () => {
+        if(result.length){
+          for(let player of result){
             expect(player.user).toBeTruthy
             expect(player.score).toBeTruthy
           }
         }
         
       })
-
-      
-
-
     })
 
 
 
 
-});
+
 
 
