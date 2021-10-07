deb5744fb0ac5146cbe2609e6527a9e0050fbc0d
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-10-07 15:57:07 +0300

    change mode back to development

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
@@ -46,8 +47,7 @@ module.exports = {
     new webpack.ProgressPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({ template: './src/index.html' }),
-    new CompressionPlugin({
-      test: /\.mp3$/,
-    }),
+    new CompressionPlugin({test: /\.mp3$/,})
   ],
 };
+
