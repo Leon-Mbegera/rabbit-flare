c50d7f8dc16d084b06cc19fa3b3cfe5c096462aa
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-10-07 13:57:18 +0300

    Resolve asset size

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
